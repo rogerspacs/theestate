3390ccc90037436a10c3b9f0e1d35bfd84d91230
rogerspecs <mwambalarogers210@gmail.com>
2021-08-11 13:45:29 +0300

    Start of admin asset listing functionality transformation

--- application/views/owner_listing.php
+++ application/views/owner_listing.php
@@ -2,7 +2,7 @@
 		<!-- Create asset section -->
 		<div class="container-fluid listings">
 			
-			<div class="row bgtranslucent">
+			<div class="row ">
 				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
 					<div class="listing_head ">
 						<div class="listing_head ">
@@ -13,152 +13,10 @@
 								<i class="lab la-gitter Clwhite bg5 size2"></i>
 								<i class="lab la-opencart Clwhite bg1 size1"></i>
 							</div>
-							<span class="Clwhite"><b>Your asset listings</b></span>
+							<span class="Clgray"><b>Your asset listings</b></span>
 						</div>
 					</div>
 				</div>
 				
 			</div>
-			<div class="row bgtranslucent">
-				<div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-					<a href="#" class="listing">
-						<div class="listing_img">
-							<i class="las la-heart Clyellow"></i>
-							<img src="<?php echo base_url();?>resources/images/1c.jpg" class="img-fluid br1">
-						</div>
-						<div class="cat">
-							<span>House for sale <br>	<b>UGX 7,780,000</b></span>
-							<span>
-								<b>4 </b>bed <b>3 </b>bath <b>1,926 </b>sqft <b>1 </b> acre plot 75-5341 keopu pi holualoa, hi 96725
-								<br><br><b class="bg1 adjust">click for more</b> <b>upload date: 01/05/2021</b>
-							</span>
-						</div>
-					</a>
-				</div>
-				<div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-					<a href="#" class="listing">
-						<div class="listing_img">
-							<i class="las la-heart Clyellow"></i>
-							<img src="<?php echo base_url();?>resources/images/1a.jpg" class="img-fluid br1">
-						</div>
-						<div class="cat">
-							<span>House for sale <br>	<b>UGX 7,780,000</b></span>
-							<span>
-								<b>4 </b>bed <b>3 </b>bath <b>1,926 </b>sqft <b>1 </b> acre plot 75-5341 keopu pi holualoa, hi 96725
-								<br><br><b class="bg1 adjust">click for more</b> <b>upload date: 01/05/2021</b>
-							</span>
-						</div>
-					</a>
-				</div>
-				<div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-					<a href="#" class="listing">
-						<div class="listing_img">
-							<i class="las la-heart Clyellow"></i>
-							<img src="<?php echo base_url();?>resources/images/1b.jpg" class="img-fluid br1">
-						</div>
-						<div class="cat">
-							<span>House for sale <br>	<b>UGX 7,780,000</b></span>
-							<span>
-								<b>4 </b>bed <b>3 </b>bath <b>1,926 </b>sqft <b>1 </b> acre plot 75-5341 keopu pi holualoa, hi 96725
-								<br><br><b class="bg1 adjust">click for more</b> <b>upload date: 01/05/2021</b>
-							</span>
-						</div>
-					</a>
-				</div>
-				<div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-					<a href="#" class="listing">
-						<div class="listing_img">
-							<i class="las la-heart Clyellow"></i>
-							<img src="<?php echo base_url();?>resources/images/1c.jpg" class="img-fluid br1">
-						</div>
-						<div class="cat">
-							<span>House for sale <br>	<b>UGX 7,780,000</b></span>
-							<span>
-								<b>4 </b>bed <b>3 </b>bath <b>1,926 </b>sqft <b>1 </b> acre plot 75-5341 keopu pi holualoa, hi 96725
-								<br><br><b class="bg1 adjust">click for more</b> <b>upload date: 01/05/2021</b>
-							</span>
-						</div>
-					</a>
-				</div>
-				<div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-					<a href="#" class="listing">
-						<div class="listing_img">
-							<i class="las la-heart Clyellow"></i>
-							<img src="<?php echo base_url();?>resources/images/1c.jpg" class="img-fluid br1">
-						</div>
-						<div class="cat">
-							<span>House for sale <br>	<b>UGX 7,780,000</b></span>
-							<span>
-								<b>4 </b>bed <b>3 </b>bath <b>1,926 </b>sqft <b>1 </b> acre plot 75-5341 keopu pi holualoa, hi 96725
-								<br><br><b class="bg1 adjust">click for more</b> <b>upload date: 01/05/2021</b>
-							</span>
-						</div>
-					</a>
-				</div>
-				<div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-					<a href="#" class="listing">
-						<div class="listing_img">
-							<i class="las la-heart Clyellow"></i>
-							<img src="<?php echo base_url();?>resources/images/1a.jpg" class="img-fluid br1">
-						</div>
-						<div class="cat">
-							<span>House for sale <br>	<b>UGX 7,780,000</b></span>
-							<span>
-								<b>4 </b>bed <b>3 </b>bath <b>1,926 </b>sqft <b>1 </b> acre plot 75-5341 keopu pi holualoa, hi 96725
-								<br><br><b class="bg1 adjust">click for more</b> <b>upload date: 01/05/2021</b>
-							</span>
-						</div>
-					</a>
-				</div>
-				<div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-					<a href="#" class="listing">
-						<div class="listing_img">
-							<i class="las la-heart Clyellow"></i>
-							<img src="<?php echo base_url();?>resources/images/1b.jpg" class="img-fluid br1">
-						</div>
-						<div class="cat">
-							<span>House for sale <br>	<b>UGX 7,780,000</b></span>
-							<span>
-								<b>4 </b>bed <b>3 </b>bath <b>1,926 </b>sqft <b>1 </b> acre plot 75-5341 keopu pi holualoa, hi 96725
-								<br><br><b class="bg1 adjust">click for more</b> <b>upload date: 01/05/2021</b>
-							</span>
-						</div>
-					</a>
-				</div>
-				<div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-					<a href="#" class="listing">
-						<div class="listing_img">
-							<i class="las la-heart Clyellow"></i>
-							<img src="<?php echo base_url();?>resources/images/1c.jpg" class="img-fluid br1">
-						</div>
-						<div class="cat">
-							<span>House for sale <br>	<b>UGX 7,780,000</b></span>
-							<span>
-								<b>4 </b>bed <b>3 </b>bath <b>1,926 </b>sqft <b>1 </b> acre plot 75-5341 keopu pi holualoa, hi 96725
-								<br><br><b class="bg1 adjust">click for more</b> <b>upload date: 01/05/2021</b>
-							</span>
-						</div>
-					</a>
-				</div>
-			</div>
-
-			<nav style="/*position: absolute;bottom: 1rem;z-index: 2;*/ margin: 0 auto;">
-			  <ul class="pagination  justify-content-center">
-			    <li class="page-item">
-			      <a class="page-link bg1 Clwhite" href="#" aria-label="Previous">
-			        <span aria-hidden="true">&laquo;</span>
-			        <span class="sr-only">Previous</span>
-			      </a>
-			    </li>
-			    <li class="page-item"><a class="page-link bg1 Clwhite" href="#">1</a></li>
-			    <li class="page-item"><a class="page-link bg1 Clwhite" href="#">2</a></li>
-			    <li class="page-item"><a class="page-link bg1 Clwhite" href="#">3</a></li>
-			    <li class="page-item">
-			      <a class="page-link bg1 Clwhite" href="#" aria-label="Next">
-			        <span aria-hidden="true">&raquo;</span>
-			        <span class="sr-only">Next</span>
-			      </a>
-			    </li>
-			  </ul>
-			</nav>
 		</div>
